971c61330a7ebf152a597b670209a8e0c2679252
George Antoniadis <george@noodles.gr>
2017-06-13 23:52:14 +0100

    Add docs and more env vars

---
+++ README.md
@@ -0,0 +1,23 @@
+# Claymore Eth Miner for NVIDIA GPUs
+
+This is a very crude container based on nvidia's cuda containers
+and the Claymore Eth dual miner.  
+It is mainly meant to be used for dual mining eth/sia on nanopool.  
+If you need to mine something different or somewhere else you might
+need to modify some more env vars or even the entrypoint script.
+
+## Running
+
+You need to specify a number or required env vars.
+
+* `EMAIL` Your email.
+* `WORKER` Your worker name.
+* `EWAL` Ethereum wallet address.
+* `DWAL` Sia wallet address.
+
+Check out the `Dockerfile` for the rest of the optional env vars supported.  
+Claymore supports quite a few more that are not being used here.
+
+```
+docker run -e "EMAIL=george@noodles.gr" -e "WORKER=docker1" -e "EWAL=0x54299f872c36f7bc306be3425abc41a5ab082752" -e "DWAL=d2ce0ab59206119c0ba30b1592c8c43a6c3f949cd1eafc41cba7791d5f74d801a4e5bac8f463" geoah/docker-claymore
+```
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib/cuda:/usr/lib/nvidia
 
-echo "POOL: eth-eu2.nanopool.org:9999, WALLET: ${EWAL}/${WORKER}/${EMAIL}, PSW: x, WORKER: , ESM: 0, ALLPOOLS: 0" > /claymore/epools.txt
-echo "POOL: eth-asia1.nanopool.org:9999, WALLET: ${EWAL}/${WORKER}/${EMAIL}, PSW: x, WORKER: , ESM: 0, ALLPOOLS: 0" >> /claymore/epools.txt
-echo "POOL: eth-us-east1.nanopool.org:9999, WALLET: ${EWAL}/${WORKER}/${EMAIL}, PSW: x, WORKER: , ESM: 0, ALLPOOLS: 0" >> /claymore/epools.txt
-echo "POOL: eth-us-west1.nanopool.org:9999, WALLET: ${EWAL}/${WORKER}/${EMAIL}, PSW: x, WORKER: , ESM: 0, ALLPOOLS: 0" >> /claymore/epools.txt
+if [[ -z "$WORKER" || -z "$EMAIL" || -z "EWAL" ]]; then
+    echo "You must at least set WORKER, EMAIL, and EWAL env vars."
+    exit 1
+fi
 
-echo "POOL: sia-eu2.nanopool.org:7777, WALLET: ${DWAL}/${WORKER}/${EMAIL}, PSW: x, WORKER: , ESM: 0, ALLPOOLS: 0" > /claymore/dpools.txt
-echo "POOL: sia-asia1.nanopool.org:7777, WALLET: ${DWAL}/${WORKER}/${EMAIL}, PSW: x, WORKER: , ESM: 0, ALLPOOLS: 0" >> /claymore/dpools.txt
-echo "POOL: sia-us-east1.nanopool.org:7777, WALLET: ${DWAL}/${WORKER}/${EMAIL}, PSW: x, WORKER: , ESM: 0, ALLPOOLS: 0" >> /claymore/dpools.txt
-echo "POOL: sia-us-west1.nanopool.org:7777, WALLET: ${DWAL}/${WORKER}/${EMAIL}, PSW: x, WORKER: , ESM: 0, ALLPOOLS: 0" >> /claymore/dpools.txt
+echo "POOL: ${EPOOL}, WALLET: ${EWAL}/${WORKER}/${EMAIL}, PSW: ${EPSW}, WORKER: ${WORKER}, ESM: ${ESM}, ALLPOOLS: 0" > /claymore/epools.txt
+echo "POOL: ${DPOOL}, WALLET: ${DWAL}/${WORKER}/${EMAIL}, PSW: ${DPSW}, WORKER: ${WORKER}, ESM: 0, ALLPOOLS: 0" > /claymore/dpools.txt
 
 /claymore/ethdcrminer64 \
     -epool ${EPOOL} \
     -ewal ${EWAL}/${WORKER}/${EMAIL} \
     -epsw ${EPSW} \
+    -ethi ${ETHI} \
+    -esm ${ESM} \
     -dcoin ${DCOIN} \
     -dpool ${DPOOL} \
     -dwal ${DWAL}/${WORKER}/${EMAIL} \
     -dpsw ${DPWD} \
-    -ethi ${ETHI} \
-    -gser ${GSER} \
+    -dcri ${DCRI} \
     -di ${DI} \
+    -gser ${GSER} \
     -dbg 0
 
